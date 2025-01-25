d6227625f2e2d99964dc91545a0e47c1344e7283
Szymon <grafiks11@gmail.com>
2025-01-25 18:55:24 +0100

    audiomanager i szymon is in

---
+++ SZYMON/music_changer_area.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var target_track: String  # Ustaw, który utwór ma być odtwarzany w tym obszarze
+
+func _on_body_entered(body: Node2D) -> void:
+	var ship := body as Ship
+	print(ship)
+	if ship:
+		AudioManager.change_music_to(target_track)
