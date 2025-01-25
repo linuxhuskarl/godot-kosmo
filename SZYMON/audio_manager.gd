32cd8471283c47a5f96f1adce453e2fa492213f3
Szymon <grafiks11@gmail.com>
2025-01-25 21:27:05 +0100

    EngineSounds

--- SZYMON/audio_manager.gd
+++ SZYMON/audio_manager.gd
@@ -1,7 +1,9 @@
 class_name AudioManagerClass extends Node
 
 @onready var music_player: AudioStreamPlayer = $MusicPlayer
+@onready var engine_player: AudioStreamPlayer = $EngineSounds
 
+var engine_on := false
 var current_track: String = ""
 # Funkcja zmieniająca muzykę z przejściem
 func change_music_to(track: String):
@@ -23,3 +25,22 @@ func change_music_to(track: String):
 		print("Stopuję muzykę")
 		music_player.stop()
 		current_track = ""
+
+
+func _process(delta):
+	var btn_press = Input.is_action_pressed("left") or Input.is_action_pressed("right") \
+		or Input.is_action_pressed("left_reverse") or Input.is_action_pressed("right_reverse")
+	if not engine_on and btn_press:
+		EngineOn();
+	elif engine_on and not btn_press:
+		EngineOff();
+
+func EngineOn():
+	engine_on = true
+	print("Wciśnięto klawisze lewo albo prawo!");
+	engine_player.set("parameters/switch_to_clip", "EngineOn");
+
+func EngineOff():
+	engine_on = false
+	print("Puszczony klawisz A/D");
+	engine_player.set("parameters/switch_to_clip", "EngineOff");
